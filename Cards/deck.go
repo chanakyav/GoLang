d641f0c4c7cd04f64d2a5c47ad129fd2863f92e5
chanakya <chanakya.valluri@gmail.com>
2018-03-02 20:30:44 -0800

    Added shuffle function for deck type

--- Cards/deck.go
+++ Cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -48,13 +50,22 @@ func (d deck) saveToFile(filename string) error {
 func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
 	if err != nil {
-		// Option #1 log the error and return a call to the newDeck()
-		// Option #2 log the error and quit the program entirely
-
 		fmt.Println("ERROR: ", err)
 		os.Exit(1)
 	}
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
