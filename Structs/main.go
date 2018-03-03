a343743ae40878e98b6870bd42a4c655f4988024
chanakya <chanakya.valluri@gmail.com>
2018-03-03 01:32:29 -0800

    Added updateName function that uses pointers

--- Structs/main.go
+++ Structs/main.go
@@ -4,12 +4,35 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Nick", lastName: "Val"}
-	fmt.Println(alex)
+	nick := person{
+		firstName: "Chanakya",
+		lastName:  "Val",
+		contactInfo: contactInfo{
+			email:   "cvalluri@email.com",
+			zipCode: 95129,
+		},
+	}
+
+	nick.updateName("Nick")
+	nick.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
