5b39b58839bb71e611a14708468a94fb1867f210
chanakya <chanakya.valluri@gmail.com>
2018-03-02 22:14:07 -0800

    Added tests and cleaned up main

--- Cards/deck.go
+++ Cards/deck.go
@@ -11,7 +11,6 @@ import (
 
 // Create a new type of 'deck'
 // which is a slice of strings
-
 type deck []string
 
 func newDeck() deck {
@@ -22,7 +21,7 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, suit+" of "+value)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 
